38633435f899f9f12774d762833c14f723a48767
cgeap <ctec@cge.ap.gov.br>
2022-01-01 18:13:56 -0300

    documentation updated

--- csv2sql.php
+++ csv2sql.php
@@ -1,27 +1,5 @@
 <?php
 
-/**
-* Autor: Josué Barros da Silva
-* Website: joshuawebdev.wordpress.com
-* Email: josue.barros1986@gmail.com
-* Versão 1.3
-*
-* Lê um arquivo no formato csv ao qual consiste em uma tabela
-* importada de um banco de dados qualquer
-* linha do arquivo .csv contém os atributos da tabela
-* A segunda linha em diante contém os dados de cada registro da tabela
-* A primeira linha é dividida e transformada em um array
-* onde seus elementos são os atributos da tabela
-* As demais linhas do arquivo são convertidas em arrays bidimensionais
-* onde cada elemento do array é um dado da tabela
-*
-* É possível salvar o conteúdo em outro arquivo por meio do
-* comando php csv2sql.php [nome_arquivo].csv [nome_tabela] > [nome_arquivo].sql
-* onde [nome_arquivo].csv é o arquivo que será submetido
-* [nome_tabela] é o nome da tabela que será gerada com os dados de [arquivo].csv
-* e [nome_arquivo].sql é o arquivo contendo as queries geradas pelo csv2sq
-*/
-
 require __DIR__ . '/vendor/autoload.php';
 
 use JoshuaWebDev\Csv2Sql\Csv2Sql;
